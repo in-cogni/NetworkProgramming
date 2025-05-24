9546d215ca4305a32512d5d1c4c6063a23f98bc0
LadaKashubo <lkashubo@yandex.ru>
2025-05-24 23:32:46 +0300

    IPcalc started

---
+++ IP calc/main.cpp
@@ -0,0 +1,26 @@
+#include<Windows.h>
+#include"resource.h"
+
+#pragma warning(disable : 28251)//чтобы WinMain не подчеркивалась 
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
