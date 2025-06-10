685f22301fdc701b4c70c49cb0d7970594a5448a
LadaKashubo <lkashubo@yandex.ru>
2025-06-11 00:54:03 +0300

    Clients identification on Server DONE

--- WinSockServer/WinSockServer.cpp
+++ WinSockServer/WinSockServer.cpp
@@ -1,4 +1,12 @@
-#include<WS2tcpip.h>More actions
+//WinSockServerMore actions
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
+
+#include<Windows.h>
+#include<WinSock2.h>
+#include<WS2tcpip.h>
 #include<iphlpapi.h>
 #include<iostream>
 using namespace std;
@@ -84,30 +92,31 @@ void main()
 		return;
 	}
 
+
 	while (true)
 	{
 		SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
-			if (g_connected_clients_count < MAX_CLIENTS)
+		if (g_connected_clients_count < MAX_CLIENTS)
+		{
+			//HandleClient(ClientSocket);
+			clients[g_connected_clients_count] = ClientSocket;
+			hThreads[g_connected_clients_count] =
+				CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)HandleClient, (LPVOID)clients[g_connected_clients_count], 0, &dwThreadIDs[g_connected_clients_count]);
+			g_connected_clients_count++;
+		}
+		else
+		{
+			CHAR receive_buffer[DEFAULT_BUFFER_LENGTH] = {};
+			INT iResult = recv(ClientSocket, receive_buffer, DEFAULT_BUFFER_LENGTH, 0);
+			if (iResult > 0)
 			{
-				//HandleClient(ClientSocket);
-				clients[g_connected_clients_count] = ClientSocket;
-				hThreads[g_connected_clients_count] =
-					CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)HandleClient, (LPVOID)clients[g_connected_clients_count], 0, &dwThreadIDs[g_connected_clients_count]);
-				g_connected_clients_count++;
+				cout << "Bytes received: " << iResult << endl;
+				cout << "Message: " << receive_buffer << endl;
+				//CHAR SZ_SORRY[] = "Sorry, but all is busy";
+				INT iSendResult = send(ClientSocket, SZ_SORRY, strlen(SZ_SORRY), 0);
+				closesocket(ClientSocket);
 			}
-			else
-			{
-				CHAR receive_buffer[DEFAULT_BUFFER_LENGTH] = {};
-				INT iResult = recv(ClientSocket, receive_buffer, DEFAULT_BUFFER_LENGTH, 0);
-				if (iResult > 0)
-				{
-					cout << "Bytes received: " << iResult << endl;
-					cout << "Message: " << receive_buffer << endl;
-					//CHAR SZ_SORRY[] = "Sorry, but all is busy";
-					INT iSendResult = send(ClientSocket, SZ_SORRY, strlen(SZ_SORRY), 0);
-					closesocket(ClientSocket);
-				}
-			}
+		}
 	}
 
 	WaitForMultipleObjects(MAX_CLIENTS, hThreads, TRUE, INFINITE);
@@ -119,6 +128,7 @@ void main()
 
 VOID WINAPI HandleClient(SOCKET ClientSocket)
 {
+	BOOL init = TRUE;
 	SOCKADDR_IN peer;
 	CHAR address[16] = {};
 	INT address_length = 16;
@@ -136,13 +146,26 @@ VOID WINAPI HandleClient(SOCKET ClientSocket)
 	/////////////////////////
 	INT iResult = 0;
 	//6) Зацикливаем Сокет на получение соединений от клиентов:
+	CHAR nickname[32]{};
 	CHAR recvbuffer[DEFAULT_BUFFER_LENGTH] = {};
+	CHAR sendbuffer[DEFAULT_BUFFER_LENGTH] = {};
 	int recv_buffer_lennth = DEFAULT_BUFFER_LENGTH;
 	do
 	{
 		ZeroMemory(recvbuffer, sizeof(recvbuffer));
+		ZeroMemory(sendbuffer, sizeof(sendbuffer));
 		//iResult = recv(ClientSocket, recvbuffer, recv_buffer_lennth, 0);
 		iResult = recvfrom(ClientSocket, recvbuffer, recv_buffer_lennth, 0, (SOCKADDR*)&peer, &address_length);
+		if (init)
+		{
+			strcpy(nickname, strrchr(recvbuffer, ' '));
+			sprintf(sendbuffer, "%s connected from [%s:%i]", nickname, address, port);
+			init = FALSE;
+		}
+		else
+		{
+			sprintf(sendbuffer, "%s[%s:%i] - %s", nickname, address, port, recvbuffer);
+		}
 		if (iResult > 0)
 		{
 			inet_ntop(AF_INET, &peer.sin_addr, address, INET_ADDRSTRLEN);
@@ -150,27 +173,26 @@ VOID WINAPI HandleClient(SOCKET ClientSocket)
 				/*<< (int)peer.sin_addr.S_un.S_un_b.s_b1 << "."
 				<< (int)peer.sin_addr.S_un.S_un_b.s_b2 << "."
 				<< (int)peer.sin_addr.S_un.S_un_b.s_b3 << "."
-			    << (int)peer.sin_addr.S_un.S_un_b.s_b4
-				<< endl; */
-				cout << "Bytes received from " << address << ":" << port << " - " << iResult << " - ";
+				<< (int)peer.sin_addr.S_un.S_un_b.s_b4
+				<< endl;*/
+			cout << "Bytes received from " << address << ":" << port << " - " << iResult << " - ";
 			//cout << "Message: " << recvbuffer << endl;
 			cout << recvbuffer << endl;
 			CHAR sz_responce[] = "Hello, I am Server! Nice to meet you!";
 			//INT iSendResult = send(ClientSocket, sz_responce, sizeof(sz_responce), 0);
-				for (int i = 0; i < g_connected_clients_count; i++)
+			for (int i = 0; i < g_connected_clients_count; i++)
+			{
+				INT iSendResult = send(clients[i], sendbuffer, strlen(sendbuffer), 0);
+				if (iSendResult == SOCKET_ERROR)
 				{
-					
-					INT iSendResult = send(clients[i], recvbuffer, strlen(recvbuffer), 0);
-					if (iSendResult == SOCKET_ERROR)
-					{
-						cout << "Error: Send failed with code: " << WSAGetLastError() << endl;
-						closesocket(ClientSocket);
-						//closesocket(ListenSocket);
-						//freeaddrinfo(result);
-						//WSACleanup();
-						//return;
-					}
+					cout << "Error: Send failed with code: " << WSAGetLastError() << endl;
+					closesocket(ClientSocket);
+					//closesocket(ListenSocket);
+					//freeaddrinfo(result);
+					//WSACleanup();
+					//return;
 				}
+			}
 			//cout << "Bytes sent: " << iSendResult << endl;
 		}
 		else if (iResult == 0)
